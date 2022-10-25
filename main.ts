309b299b2b3d2499189912e6e0d55e09d1829f9c
eileen2211 <116615780+eileen2211@users.noreply.github.com>
2022-10-24 23:45:38 -0500

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(input.lightLevel())
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Apagar luces") {
+        music.startMelody(music.builtInMelody(Melodies.Prelude), MelodyOptions.Forever)
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
 basic.forever(function () {
-    if (input.lightLevel() < 100) {
+    if (input.lightLevel() < 100 && input.buttonIsPressed(Button.A)) {
         basic.showLeds(`
             # . # . #
             . # # # .
@@ -7,6 +22,18 @@ basic.forever(function () {
             . # # # .
             # . # . #
             `)
+    } else {
+        basic.clearScreen()
+    }
+    if (input.lightLevel() > 100 && input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+        radio.sendString("Apagar luces")
     } else {
         basic.clearScreen()
     }
